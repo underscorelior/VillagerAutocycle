9a127ea3809f3fe43cc4b8a595884890d14da49f
underscorelior <newlioryt@gmail.com>
2023-09-10 22:48:55 -0700

    Initial commit

---
+++ src/main/java/wtf/underscore/villagerautocycle/VillagerAutocycle.java
@@ -0,0 +1,16 @@
+package wtf.underscore.villagerautocycle;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class VillagerAutocycle implements ModInitializer {
+    public static final String MOD_ID = "villagerautocycle";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		LOGGER.info("Hello Fabric world!");
+	}
+}
---
+++ src/main/java/wtf/underscore/villagerautocycle/VillagerAutocycleClient.java
@@ -0,0 +1,9 @@
+package wtf.underscore.villagerautocycle;
+
+import net.fabricmc.api.ClientModInitializer;
+public class VillagerAutocycleClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
